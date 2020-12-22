89f2445b795b7afcc053043ed8f415b40a811045
sebahattin <sebahattin@veriyaz.com.tr>
2020-12-23 01:17:59 +0300

    firebase added

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCAkB2tD_YMLpnJff-H2jQIH1Dg8au49xo",
-    authDomain: "narhukukdanismanlikdb.firebaseapp.com",
-    projectId: "narhukukdanismanlikdb",
-    storageBucket: "narhukukdanismanlikdb.appspot.com",
-    messagingSenderId: "131333105752",
-    appId: "1:131333105752:web:59e09fe0ce0e833649022d",
-    measurementId: "G-6WCZTXRX2V"
+    apiKey: "AIzaSyB3beWjzpM7O5XPz18hG7EeP6vP8GGK1Ow",
+    authDomain: "chineese-society.firebaseapp.com",
+    projectId: "chineese-society",
+    storageBucket: "chineese-society.appspot.com",
+    messagingSenderId: "858043187584",
+    appId: "1:858043187584:web:2021572280dd264f520165",
+    measurementId: "G-85GP8JR3P3"
 });
 
 const db = firebaseApp.firestore();
