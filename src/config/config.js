da0f1344ed4352342650533bcefad8814e7acffc
beratisahin <beratisahin94@gmail.com>
2026-05-22 01:49:55 +0300

    test render.com

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ export const config = () => {
         }
         :
         {
-            /* API_URL: 'https://sebertech-production.up.railway.app/', */
+            /* API_URL: 'https://sebertech-production.up.railway.app/api', */
 
             API_URL: 'https://sebertech.onrender.com/api',
         }
