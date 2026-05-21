053a643feb0e70736808fd5380d52c2e52a2014b
beratisahin <beratisahin94@gmail.com>
2026-05-22 01:02:23 +0300

    railway api url was change

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,6 @@ export const config = () => {
         }
         :
         {
-            API_URL: 'https://sebertech-production.up.railway.app/',
+            API_URL: 'https://sebertech-production.up.railway.app/api',
         }
 }
